dc4b551925395b9a39f9ce457ffcdc9829f83470
Arie Sandy Ibnu Karim <arie.karim@jatis.com>
2023-01-05 13:39:09 +0700

    first

---
+++ app-ireksadana/src/main/java/com/ariekarim/appireksadana/AppIreksadanaApplication.java
@@ -0,0 +1,13 @@
+package com.ariekarim.appireksadana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppIreksadanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppIreksadanaApplication.class, args);
+	}
+
+}
---
+++ app-ireksadana/src/test/java/com/ariekarim/appireksadana/AppIreksadanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariekarim.appireksadana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppIreksadanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
